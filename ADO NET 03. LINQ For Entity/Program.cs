1e617ab1455ba487835745d4558a77aef7bbe4bf
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-02-28 10:17:15 +0400

    LINQ to Entities

--- ADO NET 03. LINQ For Entity/Program.cs
+++ ADO NET 03. LINQ For Entity/Program.cs
@@ -51,8 +51,136 @@ using LibraryContext db = new();
 //    .ForEach(Console.WriteLine);
 
 //db.Books
-//    .Where(b=> EF.Functions.Like(b.Name, "%[0-9]%[0-9]%[0-9]%"))
+//    .Where(b => EF.Functions.Like(b.Name, "%[0-9]%[0-9]%[0-9]%"))
 //    .ToList()
-//    .ForEach(b=> Console.WriteLine(b.Name));
+//    .ForEach(b => Console.WriteLine(b.Name));
 #endregion
+
+#region First, FirstOrDefault, Single, SingleOrDefault, Find
+//var author = db.Authors.FirstOrDefault(a=> a.FirstName == "Olega");
+//if (author is not null) Console.WriteLine(author);
+//else Console.WriteLine("Author not found");
+
+//var author1 = db.Authors.SingleOrDefault(a => a.FirstName == "Olga");
+//if (author1 is not null) Console.WriteLine(author1);
+//else Console.WriteLine("Author not found");
+
+//var author2 = db.Authors.Find(51);
+//if (author2 is not null) Console.WriteLine(author2);
+//else Console.WriteLine("Author not found");
+#endregion
+
+#region All, Any
+
+//Console.WriteLine(db.Authors.All(a=> a.FirstName.Length > 5));
+//Console.WriteLine(db.Authors.Any(a=> a.FirstName == "Olga"));
+
+#endregion
+
+#region Select
+//var authors = db.Authors.Select(a => new
+//{
+//    Id = a.Id,
+//    FullName = $"{a.FirstName} {a.LastName}",
+//    Books = a.Books
+//});
+
+//foreach (var author in authors)
+//{
+//    Console.WriteLine($"{author.Id}. {author.FullName}");
+//    foreach (var book in author.Books)
+//    {
+//        Console.WriteLine($"\t{book}");
+//    }
+//}
+#endregion
+
+#region OrderBy, OrderByDescending, ThenBy, ThenByDescending 
+//var authors = db.Authors
+//        .OrderBy(a => a.FirstName)
+//        .ThenByDescending(a => a.LastName)
+//        .ToList();
+
+///*
+//    SELECT Id, FirstName, LastName
+//    FROM Authors
+//    ORDER BY FirstName, LastName DESC
+
+// */
+
+//authors.ForEach(Console.WriteLine);
+#endregion
+
+#region Join
+//var books = db.Books.Join(db.Authors, b => b.IdAuthor, a => a.Id,
+//    (b, a) => new
+//    {
+//        Name = b.Name,
+//        Author = $"{a.FirstName} {a.LastName}"
+//    }).ToList();
+
+///*
+//    SELECT B.[Name], A.FirstName + ' ' + A.LastName AS Author
+//    FROM Books AS B
+//    JOIN Authors AS A ON B.Id_Author = A.Id 
+// */
+
+//books.ForEach(b => Console.WriteLine($"Book name: {b.Name}\nAuthor:    {b.Author}\n"));
+
+#endregion
+
+#region Take, TakeWhile, Skip, SkipWhile
+//var authors = db.Authors
+//                    .OrderBy(a=> a.FirstName)
+//                    .Skip(4)
+//                    .Take(3)
+//                    .ToList();
+
+///*
+//    SELECT *
+//    FROM Authors
+//    ORDER BY FirstName
+//    OFFSET (4) ROWS
+//    FETCH NEXT (3) ROWS ONLY 
+//*/
+
+//authors.ForEach(Console.WriteLine);
+
+
+#endregion
+
+/* 
+    https://learn.microsoft.com/en-us/dotnet/framework/data/adonet/ef/language-reference/supported-and-unsupported-linq-methods-linq-to-entities
+    LINQ to Entities:
+    
+        All
+        Any
+        Average
+        Contains
+        Count
+        First
+        FirstOrDefault
+        Single
+        SingleOrDefault
+        Select
+        Where
+        OrderBy
+        OrderByDescending
+        ThenBy
+        ThenByDescending
+        Join
+        LeftJoin
+        GroupBy
+        Except
+        Union
+        Intersect
+        Sum
+        Max
+        Min
+        Take
+        TakeWhile
+        Skip
+        SkipWhile
+        ToList
+*/
 
